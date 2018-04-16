e7b475da8674a8de247667cf459dcc88f814f2e3
spoobert <toel3my@gmail.com>
2018-04-16 01:33:43 -0700

    simple metronome using sin wave

---
+++ Assets/metronome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class metronome : MonoBehaviour {
+    public float bpm;
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.LookAt(target);
+        float z = offset + Mathf.Sin((float)AudioSettings.dspTime * bpm / 30);
+        this.transform.localEulerAngles = new Vector3(0, 0, z * 15);
+            
+	}
+}
