829948a51847627d73fad06b63340e2240d36a1b
smoloney <s_moloney@aol.com>
2018-04-21 17:26:00 -0700

    ...

---
+++ Assets/sceneswitch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneswitch : MonoBehaviour {
+
+void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
